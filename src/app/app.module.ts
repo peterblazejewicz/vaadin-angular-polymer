6ef4e34d148108868b8f3ad5418ca742c922c385
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-11-02 23:38:58 +0100

    Polymer app layout and paper elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+
+import { PolymerElement } from '@vaadin/angular2-polymer';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PolymerElement('app-header'),
+    PolymerElement('paper-icon-button')
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
+  entryComponents: [AppComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
